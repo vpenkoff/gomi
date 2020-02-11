5b118bef22479b9cb4da36f080c4a12ef57249c1
Viktor Penkov <vpenkoff@gmail.com>
2020-02-11 08:37:36 -0500

    wip flags

--- main.go
+++ main.go
@@ -5,9 +5,26 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"flag"
+	"fmt"
 )
 
 func main() {
+
+	flag_init := flag.Bool("init", false, "gomi init")
+	flag_new := flag.String("new", "", "gomi new `migration_name`")
+	flag_migrate := flag.String("migrate", "", "gomi migrate")
+	flag.Parse()
+
+	fmt.Println(flag_init)
+	fmt.Println(flag_new)
+	fmt.Println(flag_migrate)
+
+	flag.PrintDefaults()
+
+
+
+
 	dsn := "debi:t00r@/test"
 
 	db, err := sql.Open("mysql", dsn)
